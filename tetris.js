902549fe424b5b7bdc91159fcb145a8c1432fb78
mukeshjadav2025 <mukeshjadav7696@hotmail.com>
2026-04-27 16:44:50 +0530

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -215,7 +215,7 @@ function drawGame(){
 function drawPausedScreen(){
     context.fillStyle='#333'
     context.fillRect(0,0, canvas.width, canvas.height)
-    context.fillStyle='blue'
+    context.fillStyle='red'
     context.font = '32px Russo One';
     context.textAlign = "center";
     context.fillText("Paused", canvas.width /2, canvas.height /2)
